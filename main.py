e9bff93ff036587afecfab6ae6d53951639fb114
vishalgddam873 <vishal18@navgurukul.org>
2023-05-24 18:27:40 +0530

    Initial comment

---
+++ main.py
@@ -0,0 +1,74 @@
+import numpy as np
+import cv2
+
+
+confidenceThreshold = 0.1
+NMSThreshold = 0.1
+
+modelConfiguration = 'cfg/yolov3.cfg'
+modelWeights = 'yolov3.weights'
+
+labelsPath = 'coco.names'
+
+labels = open(labelsPath).read().strip().split('\n')
+
+yoloNetwork = cv2.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
+
+# Read the input video file
+video = cv2.VideoCapture("car.mp4")
+
+
+# Define infinite while loop
+while True:
+    # Read the first frame of the video
+    check, image = video.read()
+    if check:
+        image = cv2.resize(image, (0, 0), fx=1, fy=1)
+        dimensions = image.shape[:2]
+        H, W = dimensions
+
+        blob = cv2.dnn.blobFromImage(image, 1/255, (416, 416))
+        yoloNetwork.setInput(blob)
+
+        layerName = yoloNetwork.getUnconnectedOutLayersNames()
+        layerOutputs = yoloNetwork.forward(layerName)
+
+        boxes = []
+        confidences = []
+        classIds = []
+
+        for output in layerOutputs:
+            for detection in output:
+                scores = detection[5:]
+                classId = np.argmax(scores)
+                confidence = scores[classId]
+
+                if confidence > confidenceThreshold:
+                    box = detection[0:4] * np.array([W, H, W, H])
+                    (centerX, centerY,  width, height) = box.astype('int')
+                    x = int(centerX - (width/2))
+                    y = int(centerY - (height/2))
+
+                    boxes.append([x, y, int(width), int(height)])
+                    confidences.append(float(confidence))
+                    classIds.append(classId)
+
+        indexes = cv2.dnn.NMSBoxes(
+            boxes, confidences, confidenceThreshold, NMSThreshold)
+
+        for i in range(len(boxes)):
+            if i in indexes:
+                # Write condition to detect the car in the image
+                if labels[classIds[i]] == "car":
+                    x, y, w, h = boxes[i]
+                    color = (255, 0, 0)
+                    cv2.rectangle(image, (x, y), (x + w, y + h), color, 2)
+
+        cv2.imshow('Detect The Car', image)
+        cv2.waitKey(1)
+
+    # Quit the display window when the spacebar key is pressed
+    key = cv2.waitKey(1)
+    if key == 32:
+        print("Stopped")
+        break
